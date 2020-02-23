f301670f55c07556fe9da9533402df34395847ab
Jocelyne Herrera <42578429+chicjoss@users.noreply.github.com>
2020-02-23 16:46:21 -0600

    Add files via upload

---
+++ js/emailVali.js
@@ -0,0 +1,8 @@
+function emailVali() {
+
+var email_address=document.getElementById("your_email").value;
+if(email_address==""){
+  alert("Please enter Email!");
+  return false;
+}
+}
---
+++ js/subButton.js
@@ -0,0 +1,6 @@
+  
+function AlertMessage() {
+    alert("Thank you for signing up! An e-mail with class sessions will be sent out to you shortly!");
+  
+    return false;
+  }
