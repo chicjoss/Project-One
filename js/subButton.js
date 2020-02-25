420093d0dfca1c2a325bce83fb79327920f683fe
Jocelyne Herrera <42578429+chicjoss@users.noreply.github.com>
2020-02-24 21:36:45 -0600

    Update subButton.js

--- js/subButton.js
+++ js/subButton.js
@@ -1,5 +1,5 @@
 function Alert() {
-    alert("Thank you for signing up! An e-mail with class sessions will be sent out to you shortly!");
+    alert("Thank you for signing up!");
   
     return false;
   }
